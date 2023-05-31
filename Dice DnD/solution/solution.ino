2659df168214e4427fdc6e31e5fda8d6efc182a8
Vít Jevčák <you@example.com>
2023-05-31 11:40:53 +0200

    pridani tlacitka

--- Dice DnD/solution/solution.ino
+++ Dice DnD/solution/solution.ino
@@ -1,3 +1,36 @@
+#include "funshield.h"
+constexpr byte d = 0b10100001;   // d
+
+
+class Button
+{
+  public:
+    int pin;
+    Button(int Pin)
+    {
+      pin = Pin;
+    }
+    bool Pressed()
+    {
+      bool is_pressed = !digitalRead(pin);
+      if (is_pressed & (prev_state == RELEASED))
+      {
+        prev_state = PRESSED;
+        return PRESSED;
+      }
+      else if (!is_pressed)
+      {
+        prev_state = RELEASED;
+        return RELEASED;
+      }
+      return HELD;
+    }
+    private:
+    enum ButtonState{PRESSED, HELD, RELEASED};
+    ButtonState prev_state = RELEASED;
+};
+Button Buttons[3] { Button(button1_pin), Button(button2_pin), Button(button3_pin) };
+
 void setup() {
   // put your setup code here, to run once:
 
