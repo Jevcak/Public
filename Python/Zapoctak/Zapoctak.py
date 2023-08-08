29b57ab40b1441e27742db2ebceccf8733a8a794
=?UTF-8?q?V=C3=ADt=20Jev=C4=8D=C3=A1k?= <vitjevcak3@gmail.com>
2023-08-08 21:47:58 +0200

    neulozene ulozeno

--- Python/Zapoctak/Zapoctak.py
+++ Python/Zapoctak/Zapoctak.py
@@ -1,11 +1,18 @@
 class Tree:
     def __init__(self):
-        print("incializace stromu")
-        
+        print("Initialization of the tree")
 
 class Node:
     def __init__(self, parent):
         self.parent = parent
         self.left = None
         self.right = None
-        self.balance = getBalance(self)
+        self.balance = self.getBalance()
+        
+    def getBalance(self):
+        left = getHeight(self.left)
+        right = getHeight(self.right)
+        return left-right
+
+    def getHeight(self):
+        return 0
