9fe15cbdbc1c6196d791f10f0164a5487196ecb4
=?UTF-8?q?V=C3=ADt=20Jev=C4=8D=C3=A1k?= <vitjevcak3@gmail.com>
2023-08-08 21:46:50 +0200

    zmena a funkce getHeight

--- Python/Zapoctak/Zapoctak.py
+++ Python/Zapoctak/Zapoctak.py
@@ -1,1 +1,11 @@
+class Tree:
+    def __init__(self):
+        print("incializace stromu")
+        
 
+class Node:
+    def __init__(self, parent):
+        self.parent = parent
+        self.left = None
+        self.right = None
+        self.balance = getBalance(self)
