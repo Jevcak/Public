b55e97ac8b21b6f8b68901433f33887c95307299
=?UTF-8?q?V=C3=ADt=20Jev=C4=8D=C3=A1k?= <vitjevcak3@gmail.com>
2023-08-10 21:03:32 +0200

    funkce na ziskani hloubky, balance a pridavani prvku

--- Python/Zapoctak/Zapoctak.py
+++ Python/Zapoctak/Zapoctak.py
@@ -1,18 +1,56 @@
 class Tree:
-    def __init__(self):
+    def __init__(self, root):
         print("Initialization of the tree")
+        self.root = root
+    def insert(self,key):
+        self.root.insert(key)
+
+
 
 class Node:
-    def __init__(self, parent):
-        self.parent = parent
+    def __init__(self, key):
+        self.key = key
+        self.parent = None
         self.left = None
         self.right = None
         self.balance = self.getBalance()
         
     def getBalance(self):
-        left = getHeight(self.left)
-        right = getHeight(self.right)
+        if self.left != None:
+            left = self.left.getHeight()
+        else:
+            left = 0
+        if self.right != None:
+            right = self.right.getHeight()
+        else:
+            right = 0
         return left-right
 
     def getHeight(self):
-        return 0
+        if self.left != None and self.right != None:
+            return max(self.right.getHeight(),self.left.getHeight())+1
+        elif self.right == None and self.left != None:
+            return self.left.getHeight() + 1
+        elif self.right != None and self.left == None:
+            return self.right.getHeight() + 1
+        else:
+            return 1
+    def insert(self,key):
+        if self.key == key:
+            return
+        elif self.key < key:
+            if self.right != None:
+                self.right.insert(key)
+            else:
+                self.right = Node(key)
+        elif self.key > key:
+            if self.left != None:
+                self.left.insert(key)
+            else:
+                self.left = Node(key)
+        self.balance = self.getBalance()
+
+
+strom = Tree(Node(input()))
+strom.root.insert(input())
+
