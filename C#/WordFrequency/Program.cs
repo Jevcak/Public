5da03f4bb5263b02bfce23cd82c9c08fb7b153b1
Vít Jevčák <vitjevcak3@gmail.com>
2023-10-17 22:30:14 +0200

    pridani pocitani slov a vypsani unikatnich s frekvenci, v pameti O(m+n)

---
+++ C#/WordFrequency/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace WordFrequency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line;
+            string temp;
+            char[] charSeparators = new char[] { ' ', '\t', '\n'};
+            SortedDictionary<string, int> Frequency = new SortedDictionary<string, int>();
+            string file = ResolveArg.ResolveArguments(args);
+            if (file == " ") goto End;
+            try
+            {
+                using (StreamReader read = new StreamReader(file))
+                {
+                    temp = read.ReadLine();
+                    while (temp != null)
+                    {
+                        line = temp.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (string key in line)
+                        {
+                            if (Frequency.ContainsKey(key))
+                            {
+                                Frequency[key] += 1;
+                            }
+                            else Frequency.Add(key, 1);
+                        }
+                        temp = read.ReadLine();
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("File Error");
+                goto End;
+            }
+            foreach (string key in Frequency.Keys)
+            {
+                Console.WriteLine(string.Format("{0}: {1}", key, Frequency[key]));
+            }
+        End:;
+        }
+    }
+}
---
+++ C#/WordFrequency/ResolveArg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordFrequency
+{
+    internal class ResolveArg
+    {
+        public static string ResolveArguments(string[] args)
+        {
+            string file = " ";
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new Exception("Number of arguments is not equal to 1");
+                }
+                file = args[0];
+            }
+            catch
+            {
+                Console.WriteLine("Argument Error");
+            }
+            return file;
+        }
+    }
+}
