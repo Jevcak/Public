04174a7a687459824603a5e16cbeb6128339e88e
Vít Jevčák <vitjevcak3@gmail.com>
2023-10-17 22:29:29 +0200

    pridani funkcniho a trochu hezciho word countingu s pameti O(m+m)

--- C#/WordCounting/Program.cs
+++ C#/WordCounting/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace WordCounting
+﻿using System;
+using System.IO;
+
+namespace WordCounting
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string line;
+            string file;
+            file = ResolveArg.ResolveArguments(args);
+                if (file == " ")
+            {
+                goto End;
+            }
+            try
+            {
+                int i = 0;
+                using (StreamReader reader = new StreamReader(file))
+                {
+                    line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        bool word = false;
+                        foreach (char z in line)
+                        {
+                            if (z == ' ' || z == '\t' || z == '\n')
+                            {
+                                word = false;
+                            }
+                            else if (!(z == ' ' || z == '\t' || z == '\n') && !word)
+                            {
+                                i++;
+                                word = true;
+                            }
+                        }
+                        line = reader.ReadLine();
+                    }
+                }   
+                Console.WriteLine(i);
+            }
+            catch
+            {
+                Console.WriteLine("File Error");
+            }
+        End:;
         }
     }
 }
