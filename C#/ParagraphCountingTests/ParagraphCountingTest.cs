116cedf896945debd34e50b4c46dc140d166dde2
Vít Jevčák <vitjevcak3@gmail.com>
2023-10-31 19:43:52 +0100

    pridani testu na paragraph counting

---
+++ C#/ParagraphCountingTests/ParagraphCountingTest.cs
@@ -0,0 +1,20 @@
+namespace ParagraphCountingTests
+{
+    public class ParagraphCountingTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            string input;
+
+
+
+            //Act
+
+
+
+            //Assert
+        }
+    }
+}
