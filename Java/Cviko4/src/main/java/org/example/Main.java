f1d37cf7c784e3f89d86541d68ba4ccaa908c960
Vít Jevčák <vitjevcak3@gmail.com>
2023-10-31 09:56:15 +0100

    class MyString

--- Java/Cviko4/src/main/java/org/example/Main.java
+++ Java/Cviko4/src/main/java/org/example/Main.java
@@ -1,8 +1,4 @@
 package org.example;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.BufferedReader;
-import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -10,13 +6,23 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        MyString s = new MyString("hello");
+        s.append(" world");
+        s.delete(0, 1);
+        s.insert(0, 'H');
+        s.append("!");
+        System.out.println(s.ToString());
+        MyString str = new MyString("Joko");
+        System.out.println(str);
+        str.append(" Ono");
+        System.out.println(str);
+        str.insert(4, "Neni");
+        System.out.println(str);
+        str.insert(1, 'Y');
+        System.out.println(str);
+        str.delete(0,1);
+        System.out.println(str);
+        str.insert(4,' ');
+        System.out.println(str);
     }
 }
--- Java/Cviko4/src/main/java/org/example/MyString.java
+++ Java/Cviko4/src/main/java/org/example/MyString.java
@@ -1,4 +1,49 @@
 package org.example;
 
 public class MyString {
+    String string;
+    public MyString(String str) {
+        this.string = str;
+    }
+    public void append(String str) {
+        this.string += str;
+    }
+    public void insert(int pos, String str) {
+        char[] pom = string.toCharArray();
+        String temp = "";
+        for (int i = 0; i < pos; i++) {
+            temp += pom[i];
+        }
+        temp += str;
+        for (int i = pos; i < pom.length; i++) {
+            temp += pom[i];
+        }
+        this.string = temp;
+    }
+    public void insert(int pos, char ch) {
+        char[] pom = this.string.toCharArray();
+        String temp = "";
+        for (int i = 0; i < pos; i++) {
+            temp += pom[i];
+        }
+        temp += ch;
+        for (int i = pos; i < pom.length; i++) {
+            temp += pom[i];
+        }
+        this.string = temp;
+    }
+    public void delete(int pos, int length) {
+        char[] pom = this.string.toCharArray();
+        String temp = "";
+        for (int i = 0; i < pos; i++) {
+            temp += pom[i];
+        }
+        for (int i = pos+length; i < pom.length; i++) {
+            temp += pom[i];
+        }
+        this.string = temp;
+    }
+    public String ToString() {
+        return this.string;
+    }
 }
