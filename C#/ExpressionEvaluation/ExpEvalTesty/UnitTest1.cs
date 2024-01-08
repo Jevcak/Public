e5bb2d891124c85c62adef7d8335b639b471a0e7
Vít Jevčák <vitjevcak3@gmail.com>
2024-01-08 21:29:32 +0100

    funkcni reseni expression evaluation, treba zkusit lepsi navrh asi

--- C#/ExpressionEvaluation/ExpEvalTesty/UnitTest1.cs
+++ C#/ExpressionEvaluation/ExpEvalTesty/UnitTest1.cs
@@ -3,9 +3,88 @@ namespace ExpEvalTesty
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void EasyTestRecodex1()
         {
-
+            //Arrange
+            var writer = new StringWriter();
+            string input = "+ ~ 1 3";
+            var reader = new StringReader(input);
+            //Act
+            var errHandler = new ErrorHandler(writer);
+            var program = new ProgramExecutor(reader, writer);
+            errHandler.HandleErrors(program);
+            //Assert
+            Assert.Equal("2", writer.ToString());
+        }
+        [Fact]
+        public void EasyTestRecodex2()
+        {
+            //Arrange
+            var writer = new StringWriter();
+            string input = "/ + - 5 2 * 2 + 3 3 ~ 2";
+            var reader = new StringReader(input);
+            //Act
+            var errHandler = new ErrorHandler(writer);
+            var program = new ProgramExecutor(reader, writer);
+            errHandler.HandleErrors(program);
+            //Assert
+            Assert.Equal("-7", writer.ToString());
+        }
+        [Fact]
+        public void EasyTestRecodex3()
+        {
+            //Arrange
+            var writer = new StringWriter();
+            string input = "- - 2000000000 2100000000 2100000000";
+            var reader = new StringReader(input);
+            //Act
+            var errHandler = new ErrorHandler(writer);
+            var program = new ProgramExecutor(reader, writer);
+            errHandler.HandleErrors(program);
+            //Assert
+            Assert.Equal("Overflow Error", writer.ToString());
+        }
+        [Fact]
+        public void EasyTestRecodex4()
+        {
+            //Arrange
+            var writer = new StringWriter();
+            string input = "/ 100 - + 10 10 20";
+            var reader = new StringReader(input);
+            //Act
+            var errHandler = new ErrorHandler(writer);
+            var program = new ProgramExecutor(reader, writer);
+            errHandler.HandleErrors(program);
+            //Assert
+            Assert.Equal("Divide Error", writer.ToString());
+        }
+        [Fact]
+        public void EasyTestRecodex5()
+        {
+            //Arrange
+            var writer = new StringWriter();
+            string input = "+ 1 2 3";
+            var reader = new StringReader(input);
+            //Act
+            var errHandler = new ErrorHandler(writer);
+            var program = new ProgramExecutor(reader, writer);
+            errHandler.HandleErrors(program);
+            //Assert
+            Assert.Equal("Format Error", writer.ToString());
+        }
+        [Fact]
+        public void EasyTestRecodex6()
+        {
+            //Arrange
+            var writer = new StringWriter();
+            string input = "- 2000000000 4000000000";
+            var reader = new StringReader(input);
+            //Act
+            var errHandler = new ErrorHandler(writer);
+            var program = new ProgramExecutor(reader, writer);
+            errHandler.HandleErrors(program);
+            //Assert
+            Assert.Equal("Format Error", writer.ToString());
         }
     }
 }
--- C#/ExpressionEvaluation/ExpressionEvaluation/Program.cs
+++ C#/ExpressionEvaluation/ExpressionEvaluation/Program.cs
@@ -1,10 +1,108 @@
-﻿namespace ExpressionEvaluation
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+#nullable enable
+namespace ExpressionEvaluation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var errHandler = new ErrorHandler(Console.Out);
+            var program = new ProgramExecutor(Console.In, Console.Out);
+            errHandler.HandleErrors(program);
+        }
+    }   
+    public interface IErrorHandler
+    {
+        public void HandleErrors(IProgram program);
+    }
+    public interface IProgram
+    {
+        public void Run();
+    }
+    public class ProgramExecutor : IProgram
+    {
+        private Stack<int> stack;
+        string[] operators = new string[5] { "+", "-", "*", "/", "~"};
+        private TextWriter? writer;
+        private string[] input { get; init; }
+        public ProgramExecutor(TextReader reader, TextWriter writer)
+        {
+            this.writer = writer;
+            input = reader.ReadLine()!.Split(' ');
+            stack = new Stack<int>();
+        }
+        public void Run()
+        {
+            for (int i = input.Length-1; i >= 0; i--)
+            {
+                if (int.TryParse(input[i], out int k))
+                {
+                    stack.Push(k);
+                }
+                else if (operators.Contains(input[i]))
+                {
+                    switch (input[i])
+                    {
+                        case "+":
+                            stack.Push(checked(stack.Pop() + stack.Pop()));
+                            break;
+                        case "-":
+                            stack.Push(checked(stack.Pop() - stack.Pop()));
+                            break;
+                        case "*":
+                            stack.Push(checked(stack.Pop() * stack.Pop()));
+                            break;
+                        case "/":
+                            stack.Push(checked(stack.Pop() / stack.Pop()));
+                            break;
+                        case "~":
+                            stack.Push(-stack.Pop());
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            if (stack.Count != 1) throw new FormatException();
+            writer!.Write(stack.Pop());
+        }
+    }
+    public class ErrorHandler : IErrorHandler
+    {
+        private const string overFlowError = "Overflow Error";
+        private const string zeroDivisionError = "Divide Error";
+        private const string formatError = "Format Error";
+        private TextWriter? errWriter;
+        public ErrorHandler(TextWriter writer)
+        {
+            errWriter = writer;
+        }
+        public void HandleErrors(IProgram program)
+        {
+            try
+            {
+                program.Run();
+            }
+            catch (OverflowException)
+            {
+                errWriter!.Write(overFlowError);
+            }
+            catch (DivideByZeroException)
+            {
+                errWriter!.Write(zeroDivisionError);
+            }
+            catch
+            {
+                errWriter!.Write(formatError);
+            }
+            finally
+            {
+                errWriter?.Dispose();
+            }
         }
     }
 }
