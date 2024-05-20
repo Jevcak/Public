8553c9e6f986a122a6f89676159c5857c5c74f08
Vít Jevčák <vitjevcak3@gmail.com>
2024-05-20 20:13:30 +0200

    read write

---
+++ C#/ukolPraktickyLinPro/TestingFesting/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestingFesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Testovka()
+        {
+
+            //Act
+
+            //Assert
+            Assert.Equal(1, 1);
+        }
+    }
+}
--- C#/ukolPraktickyLinPro/ukolPraktickyLinPro/Program.cs
+++ C#/ukolPraktickyLinPro/ukolPraktickyLinPro/Program.cs
@@ -5,6 +5,45 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var state = new ProgramInputOutputState();
+            state.InitStream(args);
+            var generator = new Generator(state.Reader!, state.Writer!);
+            generator.Generate();
+
+            state.Dispose();
+        }
+    }
+    public class ProgramInputOutputState : IDisposable
+    {
+        public const string FileErrorMessage = "File Error";
+        public TextReader? Reader { get; private set; }
+        public TextWriter? Writer { get; private set; }
+        public void InitStream(string[] args)
+        {
+            Reader = new StreamReader(args[0]);
+            Writer = new StreamWriter(args[1]);
+        }
+        public void Dispose()
+        {
+            Reader?.Dispose();
+            Writer?.Dispose();
+        }
+    }
+    public class Generator
+    {
+        private TextReader reader;
+        private TextWriter writer;
+
+        public Generator(TextReader rdr, TextWriter wrt)
+        {
+            reader = rdr;
+            writer = wrt;
+
+        }
+        public void Generate()
+        {
+
+
         }
     }
 }
